a052e7590bbdf4be15347c1185b0b2df34acf484
youssef ennaciri <95150458+ydy4@users.noreply.github.com>
2022-05-19 14:40:24 +0100

    Create Network_Allin1.sh

---
+++ Network_Allin1.sh
@@ -0,0 +1,5 @@
+gnome-terminal --tab --title=newTab \\ -- bash -c "./tools/dtpscan.sh ;bash"
+gnome-terminal --tab --title=newTab \\ -- bash -c "python2.7 tools/miranda.py ;bash"
+gnome-terminal --tab --title=newTab \\ -- bash -c "/root/Network/evil-ssdp/evil_ssdp.py eth0 ;bash"
+/root/Network/PCredz/PCreds -i eth0
+/root/Network/PCredz/Pcredz -i eth0
